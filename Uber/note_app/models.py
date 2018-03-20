f889520bab3d86cce83d600a01f4e1cadc200a8b
Jack Bogle <jackbbb95@gmail.com>
2018-03-20 16:46:32 -0400

    Added School and Course to models.py

--- Uber/note_app/models.py
+++ Uber/note_app/models.py
@@ -8,20 +8,21 @@ class User(models.Model):
     last_name = models.CharField(max_length = 20, verbose_name = 'Last Name')
     email = models.EmailField()
     favorites = models.ManyToManyField('note_app.Note')
+    uploaded = models.ManyToManyField('note_app.Note')
+
 
     class Meta:
         ordering = ['user_name']
     
     def __str__(self):
-        return '{0}, {1}'.format(self.last_name, self.first_name)
+        return '{0} {1}'.format(self.first_name, self.last_name)
     
-    # def get_absolute_url(self):
-    #     return reverse('urlHERE', args=[str(self.user_id)])
+    
 
 class Note(models.Model):
     note_id = models.AutoField(primary_key = True)
     author = models.ForeignKey(User, on_delete = models.CASCADE)
-    school = models.CharField(max_length = 50, verbose_name = 'School')
+    school = models.ForeignKey(School, on_delete = models.CASCADE)
     title = models.CharField(max_length = 180, verbose_name = 'Title') # Can be changed to a date or whatever
     course = models.CharField(max_length = 40, verbose_name = 'Course')
     semester = models.CharField(max_length = 40, verbose_name = 'Semester')
@@ -33,6 +34,11 @@ class Note(models.Model):
     def __str__(self):
         return '{0}, {1}'.format(self.title, self.author)
 
+class School(models.Model):
+    school_id = models.AutoField(primary_key = True)
+    name = models.CharField(max_length = 50, verbose_name = "Name")
 
-
-
+class Course(models.Model):
+    course_id = models.AutoField(primary_key = True)
+    title = models.CharField(max_length = 30, verbose_name = 'Title')
+    school = models.ForeignKey(School, on_delete = models.CASCADE)
--- Uber/note_app/urls.py
+++ Uber/note_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.DashboardView.as_view(), name='dashboard'),
-    path('note/<int:pk>', views.NoteDetailView.as_view(), name='note-detail')
+    path('',views.DashboardView.as_view(), name='dashboard-view'),
+    path('note/<int:pk>', views.NoteDetailView.as_view(), name='note-view')
 ]
