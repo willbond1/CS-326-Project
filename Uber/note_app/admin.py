e73c4c9364fcfa6995fafc3890e084fc3781e5bb
Jack Bogle <jackbbb95@gmail.com>
2018-03-27 13:55:44 -0400

    Updated models and created new version of database with example data

--- Uber/note_app/admin.py
+++ Uber/note_app/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
-from .models import User, Note
+from .models import User, School, Professor, Course, Note
 
 # Register your models here.
+class UserInline(admin.TabularInline):
+    model = User
+    extra = 1
+
 class NoteInline(admin.TabularInline):
     model = Note
+    extra = 1
+
+class SchoolInline(admin.TabularInline):
+    model = School
+    extra = 1
+
+class CourseInline(admin.TabularInline):
+    model = Course
+    extra = 1
+
+class ProfessorInline(admin.TabularInline):
+    model = Professor
     extra = 1
 
 @admin.register(User)
@@ -15,4 +31,22 @@ class UserAdmin(admin.ModelAdmin):
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
     list_display = ('title','note_id','author','school','semester','course')
+    pass
+
+@admin.register(Professor)
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display = ('professor_id','first_name','last_name')
+    inlines = [CourseInline]
+    pass
+
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ('school_id','name')
+    inlines = [CourseInline, NoteInline]
+    pass
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('course_id','title')
+    inlines = [NoteInline]
     pass
--- Uber/note_app/models.py
+++ Uber/note_app/models.py
@@ -7,8 +7,8 @@ class User(models.Model):
     first_name = models.CharField(max_length = 20, verbose_name = 'First Name')
     last_name = models.CharField(max_length = 20, verbose_name = 'Last Name')
     email = models.EmailField()
-    favorites = models.ManyToManyField('note_app.Note')
-    uploaded = models.ManyToManyField('note_app.Note')
+    favorites = models.ManyToManyField('note_app.Note', related_name="favorites", blank=True)
+    uploaded = models.ManyToManyField('note_app.Note', related_name="uploaded", blank=True)
 
 
     class Meta:
@@ -16,15 +16,37 @@ class User(models.Model):
     
     def __str__(self):
         return '{0} {1}'.format(self.first_name, self.last_name)
-    
-    
+
+class School(models.Model):
+    school_id = models.AutoField(primary_key = True)
+    name = models.CharField(max_length = 50, verbose_name = "Name")
+
+    def __str__(self):
+        return '{0}'.format(self.name)
+
+class Professor(models.Model):
+    professor_id = models.AutoField(primary_key = True)
+    first_name = models.CharField(max_length = 20, verbose_name = 'First Name')
+    last_name = models.CharField(max_length = 20, verbose_name = 'Last Name')
+
+    def __str__(self):
+        return '{0} {1}'.format(self.first_name, self.last_name)
+
+class Course(models.Model):
+    course_id = models.AutoField(primary_key = True)
+    title = models.CharField(max_length = 30, verbose_name = 'Title')
+    school = models.ForeignKey(School, on_delete = models.CASCADE)
+    professor = models.ForeignKey(Professor, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return '{0}'.format(self.title)
 
 class Note(models.Model):
     note_id = models.AutoField(primary_key = True)
     author = models.ForeignKey(User, on_delete = models.CASCADE)
     school = models.ForeignKey(School, on_delete = models.CASCADE)
     title = models.CharField(max_length = 180, verbose_name = 'Title') # Can be changed to a date or whatever
-    course = models.CharField(max_length = 40, verbose_name = 'Course')
+    course = models.ForeignKey(Course, on_delete = models.CASCADE)
     semester = models.CharField(max_length = 40, verbose_name = 'Semester')
     note_file = models.URLField(verbose_name='Note URL')
     
@@ -33,18 +55,3 @@ class Note(models.Model):
 
     def __str__(self):
         return '{0}, {1}'.format(self.title, self.author)
-
-class School(models.Model):
-    school_id = models.AutoField(primary_key = True)
-    name = models.CharField(max_length = 50, verbose_name = "Name")
-
-class Course(models.Model):
-    course_id = models.AutoField(primary_key = True)
-    title = models.CharField(max_length = 30, verbose_name = 'Title')
-    school = models.ForeignKey(School, on_delete = models.CASCADE)
-    professor = models.ForeignKey(Professor, on_delete = models.CASCADE)
-
-class Professor(model.Model):
-    professor_id = models.AutoField(primary_key = True)
-    first_name = models.CharField(max_length = 20, verbose_name = 'First Name')
-    last_name = models.CharField(max_length = 20, verbose_name = 'Last Name')
