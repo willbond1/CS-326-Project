e68a8cc2f2e328178f069bbd14bfc59df27eb478
William Sullivan <willbond1@gmail.com>
2018-03-27 22:40:48 -0400

    Add files via upload
    Added forms, some new templates, and modified User (now Profile) to extend Django's native User model

--- Uber/note_app/views.py
+++ Uber/note_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import User, Note
+from django.http import HttpResponseRedirect
+
+from .forms import NoteForm, UserForm, CommentForm
 
 # Create your views here.
 # def index(request):
@@ -25,3 +28,57 @@ class DashboardView(generic.ListView):
 
 class NoteDetailView(generic.DetailView):
     model = Note
+
+class NoteCreateView(generic.edit.CreateView):
+    def upload_notes(request):
+        if request.method == "POST":
+            form = NoteForm(request.POST, request.FILES)
+            if form.is_valid():
+                cur_note = form.save(commit=False)
+                cur_note.author = request.user.profile
+                cur_note.author.uploaded.add(cur_note)
+                cur_note.save()
+                return HttpResponseRedirect("")
+            else:
+                form = NoteForm()
+    
+        return render(request, "templates/note_app/upload.html", {"form": form})
+
+class ProfileCreateView(generic.edit.CreateView):
+    @login_required
+    @transaction.atomic
+    def create_profile(request):
+        if request.method == 'POST':
+            user_form = UserForm(request.POST, instance=request.user)
+            profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+            if user_form.is_valid() and profile_form.is_valid():
+                user_form.save()
+                profile_form.save()
+                messages.success(request, _('Your profile was successfully updated!'))
+                return redirect('settings:profile')
+            else:
+                messages.error(request, _('Please correct the error below.'))
+        else:
+            user_form = UserForm(instance=request.user)
+            profile_form = ProfileForm(instance=request.user.profile)
+        return render(request, 'templates/note_app/signup.html', {
+            'user_form': user_form,
+            'profile_form': profile_form
+        })
+
+
+class CommentCreateView(generic.edit.CreateView):
+    def add_comment(request):
+        if request.method == "POST":
+            form = CommentForm(request.POST)
+            if form.is_valid():
+                cur_comm = form.save(commit=False)
+                cur_comm.author = request.user.profile
+                cur_comm.author.post_history.add(cur_comm)
+                #ASSOCIATE COMMENT AND NOTESET WITH EACH OTHER
+                form.save():
+                return redirect("")
+            else:
+                form = CommentForm()
+
+        return render(request, "", {"form": form})
