affc63bf01c75972c28164c9aed102c7ed0b96ce
Jack Bogle <jackbbb95@gmail.com>
2018-03-28 20:55:57 -0400

    Added media file routing to urls.py (images work)

--- Uber/note_app/urls.py
+++ Uber/note_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('',views.DashboardView.as_view(), name='dashboard-view'),
     path('note/<int:pk>', views.NoteDetailView.as_view(), name='note-view')
 ]
+
