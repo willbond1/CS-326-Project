d19a694712fa1e88835c99973fdfb5d23fa04061
Chigozie Ikoro <chigozie.ikoro@gmail.com>
2018-03-29 04:42:25 -0400

    Add files via upload
    Updated profile.html and used with new data model.

--- Uber/note_app/urls.py
+++ Uber/note_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('',views.DashboardView.as_view(), name='dashboard-view'),
     path('note/<int:pk>', views.NoteDetailView.as_view(), name='note-view'),
+    path('uploaded/', views.UpView.as_view(),name='upload-view'),
     path('profile/', views.ProfileView.as_view(), name = 'profile-view')
-
 ]
-
 
--- Uber/note_app/views.py
+++ Uber/note_app/views.py
@@ -18,7 +18,17 @@ from .forms import NoteForm, UserForm, CommentForm, Profile
 #         request,
 #         'index.html',
 #         context={'num_users':num_users, 'num_notes':num_notes}
-#     
+#     )
+
+class DashboardView(generic.ListView):
+    template_name = 'dashboard.html'
+    context_object_name = 'recent_notes_list'
+    queryset = Note.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(DashboardView, self).get_context_data(**kwargs)
+        context['favorite_notes_list'] = Profile.objects.all()[0].favorites.all()
+        return context
 
 class ProfileView(generic.ListView):
     template_name = 'profile.html'
@@ -31,18 +41,14 @@ class ProfileView(generic.ListView):
         context['favorite_course_notes'] = Profile.objects.all()[0].favorites.all()
         context['course_schedule'] = Profile.objects.all()[0].course_schedule.all()
         return context
-class DashboardView(generic.ListView):
-    template_name = 'dashboard.html'
-    context_object_name = 'recent_notes_list'
-    queryset = Note.objects.all()
-
-    def get_context_data(self, **kwargs):
-        context = super(DashboardView, self).get_context_data(**kwargs)
-        context['favorite_notes_list'] = Profile.objects.all()[0].favorites.all()
-        return context
 
 class NoteDetailView(generic.DetailView):
     model = Note
+
+class UpView(generic.ListView):
+   template_name = 'uploaded_notes.html'
+   context_object_name = 'recent_uploaded_notes_list'
+   queryset = Profile.objects.all()[0].uploaded.all()
 
 class NoteCreateView(generic.edit.CreateView):
     def upload_notes(request):
